6d1fd4791a1a6931603f512250d933ec93e73318
KalinkinaMaria <mashyfycya@gmail.com>
2015-04-15 23:19:55 +0300

    Пользовательский интерфейс: создан контроллер-уровень иерархии

---
+++ FHE/FHE/Controls/HierarchyLevel.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FHE.Controls
+{
+    /// <summary>
+    /// Interaction logic for HierarchyLevel.xaml
+    /// </summary>
+    public partial class HierarchyLevel : UserControl
+    {
+        private int _indexLevel;
+        public HierarchyLevel()
+        {
+            InitializeComponent();
+        }
+
+        private void Delete_Level(object sender, RoutedEventArgs e)
+        {
+            (this.Parent as StackPanel).Children.Remove(this);
+        }
+    }
+}
