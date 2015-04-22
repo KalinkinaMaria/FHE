af0726b2f61de166ab35a2992741a479b0d3d3b2
KalinkinaMaria <mashyfycya@gmail.com>
2015-04-23 01:26:21 +0300

    Пользовательский интерфейс: создано добавление вершин по клику

--- FHE/FHE/Controls/HierarchyLevel.xaml.cs
+++ FHE/FHE/Controls/HierarchyLevel.xaml.cs
@@ -28,5 +28,17 @@ namespace FHE.Controls
         {
             (this.Parent as StackPanel).Children.Remove(this);
         }
+
+        private void Grid_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            HierarchyNode addingNode = new HierarchyNode();
+            addingNode.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+            addingNode.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            addingNode.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Center;
+            addingNode.VerticalContentAlignment = System.Windows.VerticalAlignment.Center;
+            Grid.SetColumn(addingNode, this.stackNode.ColumnDefinitions.Count);
+            this.stackNode.ColumnDefinitions.Add(new ColumnDefinition());
+            this.stackNode.Children.Add(addingNode);
+        }
     }
 }
