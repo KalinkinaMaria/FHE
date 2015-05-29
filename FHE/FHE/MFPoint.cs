ba8720ac90a8a03b555e017e237f38f21e430332
KalinkinaMaria <mashyfycya@gmail.com>
2015-05-29 14:52:38 +0300

    Модель: добавлен класс точки функции принадлежности

---
+++ FHE/FHE/MFPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FHE
+{
+    class MFPoint
+    {
+        public float x
+        {
+            get;
+            set;
+        }
+        public float y
+        {
+            get;
+            private set;
+        }
+        public Dictionary<String, float> lambda
+        {
+            get;
+            private set;
+        }
+
+        public MFPoint(float x, float y, Dictionary<String, float> lambda)
+        {
+            this.x = x;
+            this.y = y;
+            this.lambda = lambda;
+        }
+    }
+}
