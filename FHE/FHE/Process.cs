38d3933901ef13e3d1fefd1bce420e4a083835e3
KalinkinaMaria <mashyfycya@gmail.com>
2015-06-10 21:02:45 +0300

    Связка: добавлен и реализован класс запуска процесса вычисления

---
+++ FHE/FHE/Process.cs
@@ -0,0 +1,31 @@
+﻿using FHE.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FHE
+{
+    class Process
+    {
+        private List<HierarchyGoal> GoalsView;
+
+        public Process(List<HierarchyGoal> Goals)
+        {
+            this.GoalsView = Goals;
+        }
+
+        public void StartCalculation()
+        {
+            List<Node> GoalsModel = ConvertModel.FromViewToModel(GoalsView);
+
+
+
+            foreach (Goal goal in GoalsModel)
+            {
+                goal.calcMembershipFunc();
+            }
+
+        }
+    }
+}
