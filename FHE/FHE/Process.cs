769c38a27149e3cc798ef905adebfaa16c3b58b8
KalinkinaMaria <mashyfycya@gmail.com>
2015-06-11 18:53:09 +0300

    Связка: реализован метод получения высчитанной функции принадлежности

--- FHE/FHE/Process.cs
+++ FHE/FHE/Process.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 
 namespace FHE
 {
     class Process
     {
         private List<HierarchyGoal> GoalsView;
+        private List<Goal> GoalsModel;
 
         public Process(List<HierarchyGoal> Goals)
         {
@@ -17,15 +19,25 @@ namespace FHE
 
         public void StartCalculation()
         {
-            List<Node> GoalsModel = ConvertModel.FromViewToModel(GoalsView);
-
-
+            GoalsModel = ConvertModel.FromViewToModel(GoalsView);
 
             foreach (Goal goal in GoalsModel)
             {
                 goal.calcMembershipFunc();
             }
 
+        }
+
+        public List<Point> GetResultMembershipFunction(int index)
+        {
+            List<Point> result = new List<Point>();
+
+            foreach (MFPoint point in GoalsModel[index].achievementGoal.points)
+            {
+                result.Add(new Point(point.x, point.y));
+            }
+
+            return result;
         }
     }
 }
